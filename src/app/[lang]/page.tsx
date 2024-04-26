31cc0fefcaf5782c58fce6b7b9c104cc06a3cf1f
Gustavo Santana <gustavossw@hotmail.com>
2024-04-26 10:08:50 -0300

    feat: add flag on language select

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -26,18 +26,20 @@ export default async function Home({ params: { lang } }: HomeProps) {
       <WorksList dictionary={dictionary.works} />
 
       <section className="flex min-h-0 flex-col gap-y-3">
-        <h1 className="text-xl font-bold">Skills</h1>
+        <h1 className="text-xl font-bold">{dictionary.skills["title"]}</h1>
         <div className="flex w-full flex-wrap gap-1">
           <SkillsList skillsItems={skillsItems} />
         </div>
       </section>
 
       <section className="flex min-h-0 flex-col gap-y-3 py-3">
-        <h1 className="text-xl font-bold">Education</h1>
+        <h1 className="text-xl font-bold">{dictionary.education["title"]}</h1>
 
         <div className="flex flex-col gap-2">
           <div className="flex items-center justify-between gap-1">
-            <h1 className="font-bold">Uninassau - Distance Learning </h1>
+            <h1 className="font-bold">
+              {dictionary.education["uninassau"].title}
+            </h1>
 
             <time>
               <span className="font-mono">2024 - 2026</span>
@@ -45,7 +47,7 @@ export default async function Home({ params: { lang } }: HomeProps) {
           </div>
 
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            Degree in systems analysis and development.
+            {dictionary.education["uninassau"].description}
           </p>
         </div>
       </section>
